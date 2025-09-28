1f274a679bb74488d395f8fed36dd4c3310530a6
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-09-28 22:40:28 +0300

    Updated index.tsx

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <header className="bg-blue-600 text-white p-4 shadow">
       <h1 className="text-2xl font-semibold">ALX Project 2 Header</h1>
+      <nav className="flex space-x-4">
+        <Link href="/">Home</Link>
+        <Link href="/home">/home</Link>
+        <Link href="/about">/about</Link>
+      </nav>
     </header>
   );
 };
